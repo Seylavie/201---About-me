7ce54f5d5aecff41bcc3482479c0643a4b71cfbc
Halle <halle.reck@gmail.com>
2019-04-18 19:15:08 -0400

    Added better appearance

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var questions = ['Do I have siblings?', 'Is my favourite colour green?'];
+var questions = ['Do I have siblings?', 'Do I live on Mars?'];
 var answers = ['Yes', 'No']
 
 
 
 //Prompt user and get a response
 var response = prompt(questions[0]);
-if (response === answers[0]) {
+if (response === answers[0])//.toLowerCase() {
   alert('You are right!');
   console.log({correct:'Yes'});
 } else {
@@ -17,3 +17,8 @@ if (response === answers[0]) {
 
 //var arrayLength = questions.length, answers.length;
 //for (i = 0; i < arrayLength; i++)
+
+/*switch (answers) {
+  case 'Yes':
+  alert('why')
+}
