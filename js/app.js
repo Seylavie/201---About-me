c87693f7d6f67e6356a86f3513fa5ef50fce2dfc
Halle <halle.reck@gmail.com>
2019-04-19 16:59:44 -0400

    Big Appearance Update

--- js/app.js
+++ js/app.js
@@ -3,22 +3,23 @@
 var questions = ['Do I have siblings ?', 'Am I from Mars?', 'Can I go fishing in the Sahara?', 'Have I ever eaten Balut? ', 'Have I ever been ice skating in the Grand Canyon?' ];
 var answers = ['yes', 'no', 'yes', 'no', 'no' ]
 
+do {
+  //Prompt user and get a response
+  var response = prompt(questions[0]).toLowerCase();
 
-//Prompt user and get a response
-var response = prompt(questions[0]).toLowerCase();
-
-//Loop over questions
-for (var i = 0; i < questions.length; i++) {
-  if (response === answers[0]) {
-    alert('You are right!');
-    console.log({correct:'Yes'});
-    break;
-  } else {
-    alert('Oh, but I do.');
-    console.log({incorrect:'No'});
-    break;
+  //Loop over questions
+  for (var i = 0; i < questions.length; i++) {
+    if (response === answers[0]) {
+      alert('You are right!');
+      console.log({correct:'Yes'});
+      break;
+    } else {
+      alert('Oh, but I do.');
+      console.log({incorrect:'No'});
+      break;
+    }
   }
-}
+} while (!correct);
 
 
 /*var response = prompt(questions[1]);
