4f0e81c6696b184ae5e4e2d791e77e37c20a0270
Kara Hunte <ncryted@gmail.com>
2019-04-20 17:06:06 -0400

    Update Guessing game and Added portfolio page

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 'use strict';
 
-var questions = ['Do I have siblings ?', 'Am I from Mars?', 'Can I go fishing in the Sahara?', 'Have I ever eaten Balut? ', 'Have I ever been ice skating in the Grand Canyon?' ];
-var answers = ['yes', 'no', 'yes', 'no', 'no' ]
+var questions = ['Do I have siblings ?', 'Am I from Mars?', 'Can I go fishing in the Sahara?', 'Have I ever eaten Balut? ', 'Have I ever been ice skating in the Grand Canyon?'];
+var answers = ['yes', 'no', 'yes', 'no', 'no']
 
 var numQuestion = ['How many hours do I sleep?']
 var numAnswer = 11
 
 //Prompt user and get a response
 
-var response = prompt(questions[0]).toLowerCase();
-if (response === answers[0]) {
-  alert('You are right!');
-  console.log({correct:'Yes'});
-} else {
-  alert('Oh, but I do.');
-  console.log({incorrect:'No'});
+for (var i = 0; i < questions.length; i++) {
+  var response = prompt(questions[i]).toLowerCase();
+  if (response === answers[i]) {
+    alert('You are right!');
+    console.log({ correct: 'Yes' });
+  } else {
+    alert('Oh, but I do.');
+    console.log({ incorrect: 'No' });
+  }
+
 }
 
+/*
 var response = prompt(questions[1]);
 if (response === answers[1]) {
   alert('Congrats! :)');
@@ -59,3 +63,4 @@ if (response1 > numAnswer) {
 } else {
   alert('That is to low.');
 }
+*/
